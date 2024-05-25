1aaa330091fcb4bc60f0ca80fdfb992bc7485d7e
Alex <alexgarciagandia304@gmail.com>
2024-05-25 18:13:07 +0200

    Configs updated

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+🛠️ Mi Biblioteca de Componentes
+¡Bienvenido a Mi Biblioteca de Componentes! Esta biblioteca contiene una colección de componentes reutilizables que puedes usar para construir tus aplicaciones web en React de manera más rápida y eficiente.
+
+🚀 Características
+Componentes reutilizables: Fácil de integrar en cualquier proyecto.
+Personalizable: Estilo y funcionalidad personalizables según tus necesidades.
+Modular: Incluye solo los componentes que necesitas.
+
+
+📦 Instalación
+Para instalar la biblioteca, puedes usar npm:
+```
+npm install mi-biblioteca-componentes
+```
+
+📚 Documentación
+Para ver la documentación completa, clona el repositorio y arranca con:
+```
+git clone https://github.com/tu-usuario/mi-biblioteca-componentes.git
+cd mi-biblioteca-componentes
+npm install
+npm run dev
+```
+Esto iniciará el storybook donde podras ver todos los componentes y sus props.
 
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
 import packageJson from './package.json' assert { type: 'json' };
+import babel from '@rollup/plugin-babel';
+
 
 export default [
   {
@@ -12,22 +13,21 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: false,
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: false,
       },
     ],
     plugins: [
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-env','@babel/preset-react']
+      }),
       resolve(),
       commonjs(),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/*.stories.tsx', '**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'],
-      }),
-      postcss(),
+      terser()
     ],
     external: ['react', 'react-dom'],
   },
